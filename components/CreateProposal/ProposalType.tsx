e90646f655a099ce5e4db875296d17393cd9d3b8
Oleg Kalenik <okalenyk@gmail.com>
2023-04-03 17:40:34 +0300

    *disabled gov token transfer for eth governance daos

--- components/CreateProposal/ProposalType.tsx
+++ components/CreateProposal/ProposalType.tsx
@@ -1,5 +1,8 @@
 import { Dispatch, SetStateAction } from 'react';
 
+import { useAtomValue } from 'jotai';
+import { currentDaoAtom } from 'store/dao';
+
 import { Typography } from 'components/ui-kit/Typography';
 import {
   Select,
@@ -16,6 +19,8 @@ type ProposalTypeProps = {
 };
 
 export function ProposalType({ setProposalType }: ProposalTypeProps) {
+  const currentDao = useAtomValue(currentDaoAtom);
+
   const onProposalTypeValueChange = (_proposalType: ProposalEnum) =>
     setProposalType(_proposalType);
 
@@ -25,11 +30,21 @@ export function ProposalType({ setProposalType }: ProposalTypeProps) {
         <SelectValue placeholder={InputLabel.PROPOSAL_TYPE} />
       </SelectTrigger>
       <SelectContent>
-        {Object.entries(ProposalEnum).map(([_proposalKey, _proposalValue]) => (
-          <SelectItem value={_proposalValue} key={_proposalKey}>
-            <Typography variant="button2">{_proposalValue}</Typography>
-          </SelectItem>
-        ))}
+        {Object.entries(ProposalEnum)
+          .filter(([_proposalKey]) => {
+            const govType = currentDao?.policy.governance.__typename;
+            switch (govType) {
+              case 'OwnershipWeightedVoting':
+                return _proposalKey !== 'PROPOSE_TRANSFER_GOVERNANCE_TOKEN';
+              default:
+                return true;
+            }
+          })
+          .map(([_proposalKey, _proposalValue]) => (
+            <SelectItem value={_proposalValue} key={_proposalKey}>
+              <Typography variant="button2">{_proposalValue}</Typography>
+            </SelectItem>
+          ))}
       </SelectContent>
     </Select>
   );
